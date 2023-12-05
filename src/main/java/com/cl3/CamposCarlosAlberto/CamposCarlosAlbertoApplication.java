0ec061ddeefff676b09e68b50ca345f373ed1fb1
TheMagus08 <i202221393@cibertec.edu.pe>
2023-12-04 22:13:16 -0500

    second commit

---
+++ src/main/java/com/cl3/CamposCarlosAlberto/CamposCarlosAlbertoApplication.java
@@ -0,0 +1,13 @@
+package com.cl3.CamposCarlosAlberto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamposCarlosAlbertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamposCarlosAlbertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cl3/CamposCarlosAlberto/CamposCarlosAlbertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl3.CamposCarlosAlberto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamposCarlosAlbertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
